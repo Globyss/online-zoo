2ce6f7bcc000ae4c80dc0e79e2c02827fb400171
Globyss <vlad_zhuk98@mail.ru>
2023-01-24 13:10:39 +0300

    feat: add backlight to 'pick and feed' value

--- pages/donate/script.js
+++ pages/donate/script.js
@@ -29,6 +29,8 @@ const imgPosition = document.querySelector('.progress-bar__icon')
 for (let item of document.querySelectorAll('.price')) {
     if (item.innerHTML === "100") {
         imgPosition.style.transform = `translateX(${item.closest('.progress-bar__button').offsetLeft - 12}px)`
+        const price = item.querySelector('.price')
+        item.classList.add('active')
     }
 }
 
@@ -50,11 +52,17 @@ function selectPoint(event) {
 const progressBarInput = document.querySelector('.progress-bar__input input[type="number"]')
 
 function setFeedValue() {
+    let selectElem
     for (let value of document.querySelectorAll('.price')) {
         if (value.innerHTML == progressBarInput.value) {
             imgPosition.style.transform = `translateX(${value.closest('.progress-bar__button').offsetLeft - 12}px)`
+            selectElem = value.closest('.progress-bar__button').querySelector('.price')
         }
     }
+    for (let item of document.querySelector(".progress-bar__background").children) {
+        if(item.querySelector('.price')) item.querySelector('.price').className = 'price'
+    }
+    selectElem.classList.add('active')
 }
 
 progressBar.addEventListener('click', selectPoint)
